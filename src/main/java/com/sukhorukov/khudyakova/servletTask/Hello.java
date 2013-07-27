1f78230a8eeed3a65fa486c5decec424f82650c5
EjeVika <khudyakovav@gmail.com>
2013-07-27 13:00:12 +0700

    ver2

--- src/main/java/com/sukhorukov/khudyakova/servletTask/Hello.java
+++ src/main/java/com/sukhorukov/khudyakova/servletTask/Hello.java
@@ -1,21 +1,37 @@
 package com.sukhorukov.khudyakova.servletTask;
 
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
 
  */
 @WebServlet( name = "SimpleServlet", urlPatterns = {"/simple"} )
 public class Hello extends HttpServlet {
+    private static final Logger log = Logger.getLogger(Hello.class);
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getOutputStream().println("Hello");
+    //    resp.getOutputStream().println("Hello");
+
+
+        List<Integer> list = new ArrayList<>();
+        for(Integer i=0;i<=10;i++){
+            list.add(i);
+        }
+
+        req.setAttribute("list",list);
         req.getRequestDispatcher("/WEB-INF/test.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/WEB-INF/test.jsp
+++ src/main/webapp/WEB-INF/test.jsp
@@ -1,13 +1,17 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
 <title>test</title>
 </head>
 <body>
-<h2>Hi!</h2>
+<h2>Hi! hi hi</h2>
 
 <p> Current date: <%= new java.util.Date()%>
 <br>
-<p> Value of parameter "param": <%= request.getParameter("param")%>
-
+<p> Value of parameter "param": ${param["param"]}
+<c:forEach var="p" items="${list}">
+<li>${p}</li>
+</c:forEach>
 </body>
 </html>
